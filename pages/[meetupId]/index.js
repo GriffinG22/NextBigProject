75d96a90b9f9af23ac8a216def967b6db6579729
Griffin Gradke <griffin.gradke@gmail.com>
2023-03-27 13:49:39 -0500

    DetailPage layout + prop structure

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetailPage = () => {
   return (
-    <div>MeetupDetailPage</div>
-  )
-}
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is outr first meetup"
+    />
+  );
+};
 
-export default MeetupDetailPage
+export default MeetupDetailPage;
