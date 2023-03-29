abd9ed498551a7426d196ee307946a6fc35c08c0
Griffin Gradke <griffin.gradke@gmail.com>
2023-03-29 12:36:33 -0500

    MongoDB connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // Only POST requests will trigger function
 
@@ -6,6 +8,10 @@ function handler(req, res) {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    MongoClient.connect(
+      "mongodb+srv://Griffin:zIhHEqqHqHhPREdM@cluster0.o8blapn.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
   }
 }
 
